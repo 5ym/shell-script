ce608ad9f37c9807da6eed2faca72d305244cc1d
Ryuki Maruyama <info@doany.io>
2026-06-17 16:00:35 +0900

    Delete cuda.sh

--- cuda.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-rm -rf cuda-keyring_1.1-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-sudo reboot
