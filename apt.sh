13f9586db3f16fdedc9335e5a19b84f0485280eb
Ryuki Maruyama <info@doany.io>
2026-06-30 14:39:22 +0900

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#パッケージ更新、自動削除してキャッシュクリーンするやつ
+#パッケージ更新、自動削除
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 #パッケージ削除済みだか残留した設定ファイルをすべて削除するやつ
 sudo apt purge -y $(dpkg -l | grep '^rc' | awk '{print $2}')
